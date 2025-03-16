74ce4424831a9fe02d20ae4624820480f4574b14
Nate Bass <nate.bass@outlook.com>
2025-03-15 17:39:10 -0700

    Refactor styles and fix bugs from introducing Tailwind.
    * Run migration tool to update SASS @import to @use.
    * Replace normalize.scss with Tailwind.
    * Fix link styling.
    * Fix get-started styling.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Home from './home/home'
 
 export default function DefaultPage() {
-  return (
-    <div className='pageContainer'>
-      <Home></Home>
-    </div>
-  )
+    return (
+        <div className='bg-[var(--color-secondary-50)]'>
+            <Home></Home>
+        </div>
+    )
 }
 
