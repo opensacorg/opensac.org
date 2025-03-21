00d95abd14bebb43ac0e7bedb8a3899f32fc086b
Nate Bass <nate.bass@outlook.com>
2025-03-21 04:19:59 -0700

    First draft of community support statement.

--- src/app/community-support-statement-2025/page.js
+++ src/app/community-support-statement-2025/page.js
@@ -1,15 +1,16 @@
-import HeaderUnderline from '../../components/layout/HeaderUnderline'
-import SidebarSection from '../../components/layout/SidebarSection'
-import AppLayout from '@/components/layout/AppLayout'
+import Link from "next/link";
+import HeaderUnderline from "../../components/layout/HeaderUnderline";
+import SidebarSection from "../../components/layout/SidebarSection";
+import AppLayout from "@/components/layout/AppLayout";
 
 /**
  * Section type. Displays light or dark themes.
  * @type {{light: string, dark: string}}
  */
 export const SectionType = {
-  light: 'light',
-  dark: 'dark',
-}
+  light: "light",
+  dark: "dark",
+};
 
 /**
  * Page for information about how to pitch a project.
@@ -26,16 +27,24 @@ export default function CommunitySupportStatementPage() {
             description="Community Support Statement"
           />
         </div>
-        <div>
+        <div className="prose">
           <p>
-            At Open Sacramento (OpenSac) and OpenFresno (OpenFresno), we are
-            dedicated to fostering a community that is safe, respectful, and
-            inclusive for all. We take our roles in the Greater Sacramento and
-            Greater Fresno areas seriously, and supporting diversity and
-            inclusion is not about taking sides—it&aposs about doing what&aposs
-            right for our community and being good people.
+            At <Link href="https://opensac.org/">Open Sacramento</Link>{" "}
+            (OpenSac) and <Link href="">OpenFresno</Link> (OpenFresno), we are
+            dedicated to{" "}
+            <b>
+              fostering a community that is safe, respectful, and inclusive for
+              all.
+            </b>{" "}
+            We take our roles in the Greater Sacramento and Greater Fresno areas
+            seriously, and supporting diversity and inclusion is not about
+            taking sides—it&apos;s about doing what&apos;s right for our
+            community and being good people.
           </p>
-          <p>Our Code of Conduct embodies these values and ensures that we:</p>
+          <p>
+            <Link href="/code-of-conduct">Our Code of Conduct</Link> embodies
+            these values and ensures that we:
+          </p>
           <ul>
             <li>
               Create a safe and respectful environment where every individual
@@ -46,7 +55,7 @@ export default function CommunitySupportStatementPage() {
               discrimination.
             </li>
             <li>
-              Embrace everyone&aposs ideas, skills, and contributions,
+              Embrace everyone&apos;s ideas, skills, and contributions,
               recognizing that diverse perspectives make us stronger.
             </li>
             <li>
@@ -59,44 +68,52 @@ export default function CommunitySupportStatementPage() {
             </li>
           </ul>
           <p>
-            OpenSac/OpenFresno isn&apost just about technology—it&aposs about
-            building a community where everyone feels heard and valued. We
-            believe in the strength of diverse teams and in creating tools that
-            serve the public good. This commitment goes beyond our work—it
+            OpenSac/OpenFresno isn&apos;t just about technology—it&apos;s about
+            building a community where <b>everyone feels heard and valued</b>.
+            We believe in the strength of diverse teams and in creating tools
+            that serve the public good. This commitment goes beyond our work—it
             extends to the relationships we build within our community and with
             others, ensuring that every voice is heard, especially those that
             have historically been excluded.
           </p>
           <p>
-            That&aposs why OpenSac and OpenFresno is a part of the Alliance of
-            Civic Technologists. In this network, we collaborate with
-            like-minded organizations to create a more diverse, equitable,
-            inclusive, and accessible future. As outlined in their{' '}
-            <a href="#" target="_blank" rel="noopener noreferrer">
-              joint statement with Technologists for the Public Good
-            </a>
+            That&apos;s why OpenSac and OpenFresno is a part of the{" "}
+            <Link href="https://www.civictechnologists.org/">
+              Alliance of Civic Technologists
+            </Link>
+            . In this network, we collaborate with like-minded organizations to
+            create a more diverse, equitable, inclusive, and accessible future.
+            As outlined in their{" "}
+            <Link href="https://mailchi.mp/civictechnologists/act-tpg-joint-statement">
+              joint statement
+            </Link>{" "}
+            with{" "}
+            <Link href="https://www.publicgood.tech/">
+              Technologists for the Public Good
+            </Link>
             , we are united in our pursuit to make a positive difference.
           </p>
           <p>
-            Today, we reaffirm our commitment to inviting and supporting
-            individuals who are working to make the Greater Sacramento and
-            Greater Fresno areas—and beyond—a better, more inclusive place. We
-            stand with all members of our community—whether you&aposre trans,
-            queer, disabled, an immigrant, a remote worker, or anyone who&aposs
-            often marginalized. We firmly believe that discrimination of any
-            kind has no place here. Whether in public spaces, the workplace, or
-            online, we will not tolerate it.
+            Today, we{" "}
+            <b>
+              reaffirm our commitment to inviting and supporting individuals who
+              are working to make the Greater Sacramento and Greater Fresno
+              areas—and beyond—a better, more inclusive place.
+            </b>{" "}
+            We stand with all members of our community—whether you&apos;re
+            trans, queer, disabled, an immigrant, a remote worker, or anyone
+            who&apos;s often marginalized. We firmly believe that discrimination
+            of any kind has no place here. Whether in public spaces, the
+            workplace, or online, we will not tolerate it.
           </p>
           <p>
-            This is not just the right thing to do—it is a vital step toward
-            improving our community for everyone.
-          </p>
-          <p>
-            <strong>Note:</strong> Replace the &apos#&apos in the link&aposs
-            href with the actual URL to the joint statement.
+            <b>
+              This is not just the right thing to do—it is a vital step toward
+              improving our community for everyone.
+            </b>
           </p>
         </div>
       </SidebarSection>
     </AppLayout>
-  )
+  );
 }
